38c8bdff48d57bc03de306b51b808e4ca332ed25
mbrenman <mattbrenman@gmail.com>
2014-02-19 23:55:13 -0500

    fixed naming issue and added basic js skeleton

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,12 @@
-// Your work goes here...
+
+function draw() {
+    canvas = document.getElementById('game');
+    // Check if canvas is supported on browser
+    if (canvas.getContext) {
+        ctx = canvas.getContext('2d');
+        //...
+    } 
+    else {
+        alert('Sorry, canvas is not supported on your browser!');
+    }
+}
