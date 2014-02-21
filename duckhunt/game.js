148eb4dc0db9bbabcf34f46d24f539dd1f35a66b
mbrenman <mattbrenman@gmail.com>
2014-02-20 22:00:46 -0500

    added a slice of the picture

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -4,7 +4,16 @@ function draw() {
     // Check if canvas is supported on browser
     if (canvas.getContext) {
         ctx = canvas.getContext('2d');
-        //...
+        var sprite_sheet = new Image();
+        sprite_sheet.onload = function(){
+        	var x = 0;
+        	var y = 0;
+        	sizex = 60;
+        	sizey = 57;
+        	ctx.drawImage(sprite_sheet, x*sizex, y*sizey, sizex, sizey, 0, 0, sizex, sizey);
+
+        };
+        sprite_sheet.src = 'assets/duckhunt.png';
     } 
     else {
         alert('Sorry, canvas is not supported on your browser!');
