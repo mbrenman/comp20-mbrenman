83dc714c5f467fe89bcd404399051ef719043c72
mbrenman <mattbrenman@gmail.com>
2014-03-13 21:44:43 -0400

    added google maps

--- transit/rodeo.js
+++ transit/rodeo.js
@@ -1,8 +1,55 @@
-function init() {
-	console.log("hey it's matt!");
-	// latlng = new google.maps.LatLng(...);
-	// "..." is stuff you have to fill in
+var myLat = 0;
+var myLng = 0;
+var request = new XMLHttpRequest();
+var me = new google.maps.LatLng(myLat, myLng);
+var myOptions = {
+			zoom: 13, // The larger the zoom number, the bigger the zoom
+			center: me,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+var map;
+var marker;
+var infowindow = new google.maps.InfoWindow();
+var places;
 
-	// myOptions = {...};
-	// map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+function init()
+{
+	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	getMyLocation();
+}
+
+function getMyLocation()
+{
+	if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+		navigator.geolocation.getCurrentPosition(function(position) {
+			myLat = position.coords.latitude;
+			myLng = position.coords.longitude;
+			renderMap();
+		});
+	}
+	else {
+		alert("Geolocation is not supported by your web browser.  What a shame!");
+	}
+}
+
+function renderMap()
+{
+	me = new google.maps.LatLng(myLat, myLng);
+
+	// Update map and go there...
+	map.panTo(me);
+
+	// Create a marker
+	marker = new google.maps.Marker({
+		position: me,
+		title: "Here I Am!"
+	});
+	marker.setMap(map);
+
+	// Open info window on click of marker
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent(marker.title);
+		infowindow.open(map, marker);
+	});
+
 }
