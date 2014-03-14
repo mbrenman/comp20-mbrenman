fee42288d15113e9895041d6cc692a80e00babc1
mbrenman <mattbrenman@gmail.com>
2014-03-13 21:36:22 -0400

    added google maps API

---
+++ transit/rodeo.js
@@ -0,0 +1,8 @@
+function init() {
+	console.log("hey it's matt!");
+	// latlng = new google.maps.LatLng(...);
+	// "..." is stuff you have to fill in
+
+	// myOptions = {...};
+	// map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+}
