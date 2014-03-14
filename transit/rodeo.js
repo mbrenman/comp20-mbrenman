5f9f0611ba5dafbebd92f8072d41c139358e7346
mbrenman <mattbrenman@gmail.com>
2014-03-14 16:29:32 -0400

    debugging the table ordering

--- transit/rodeo.js
+++ transit/rodeo.js
@@ -156,6 +156,8 @@ function makeScheduleTable(stop)
 			}
 		}
 		tableElems = tableElems.sort(function(a,b){ return a[1] - b[1] });
+		console.log(tableElems);
+		console.log("there it is!");
 		for (t in tableElems){
 			row = tableElems[t];
 			tbl += "<tr style='border: 1px solid black;'>";
